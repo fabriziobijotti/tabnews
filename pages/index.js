81f7e7d4f71466f31a4a489ecd125d67d9a289dd
Fabrizio Spegiorin Bijotti <129006253+fabriziobijotti@users.noreply.github.com>
2026-02-15 21:30:24 +0000

    Add editorconfig

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 function Home() {
     return <h1> Otávio, quando você fizer essa atividade, vai entender por que eu te enviei esse link. Quero que você me mande uma mensagem no momento certo, depois que concluir essa atividade no futuro breve!🙂</h1>
 }
-
 export default Home;
