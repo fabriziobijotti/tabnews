83688fb506995b754043d54a40d49c3b17df9067
Fabrizio Spegiorin Bijotti <129006253+fabriziobijotti@users.noreply.github.com>
2026-02-15 22:41:13 +0000

    update txt

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       {" "}
-      Otávio, quando você fizer essa atividade, vai entender por que eu te
+      Batata, quando você fizer essa atividade, vai entender por que eu te
       enviei esse link. Quero que você me mande uma mensagem no momento certo,
       depois que concluir essa atividade no futuro breve!🙂
     </h1>
