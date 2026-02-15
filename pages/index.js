38dd500f72e6d3946ee81ef249ab54fb062a06f5
Fabrizio Spegiorin Bijotti <129006253+fabriziobijotti@users.noreply.github.com>
2026-02-15 17:22:39 +0000

    Add init page

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1> Otávio, quando você fizer essa atividade, vai entender por que eu te enviei esse link. Quero que você me mande uma mensagem no momento certo, depois que concluir essa atividade no futuro.🙂</h1>
+}
+
+export default Home;
