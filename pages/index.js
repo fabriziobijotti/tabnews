44878014e32d5f633c924f94c25ddfff2703be37
Fabrizio Spegiorin Bijotti <129006253+fabriziobijotti@users.noreply.github.com>
2026-02-15 18:44:57 +0000

    Change txt

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Otávio, quando você fizer essa atividade, vai entender por que eu te enviei esse link. Quero que você me mande uma mensagem no momento certo, depois que concluir essa atividade no futuro.🙂</h1>
+    return <h1> Otávio, quando você fizer essa atividade, vai entender por que eu te enviei esse link. Quero que você me mande uma mensagem no momento certo, depois que concluir essa atividade no futuro breve!🙂</h1>
 }
 
 export default Home;
